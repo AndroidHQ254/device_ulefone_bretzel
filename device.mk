83caa45ee4adbf01c418c345fb9ff429e13de117
Kirill <vsegda_online_007@bk.ru>
2020-02-27 14:00:04 +0300

    Create vendor_props.mk

--- device.mk
+++ device.mk
@@ -56,3 +56,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.xml
+
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_props.mk
