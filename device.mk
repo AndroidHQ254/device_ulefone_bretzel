c8f44536fb69e5b1ff214a90678aa8ef4c7fdc45
FlashKillerV2 <sebasromtz123@gmail.com>
2020-07-22 14:56:09 -0500

    Fix missing symbols in libshowlogo

--- device.mk
+++ device.mk
@@ -65,5 +65,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.xml
 
+# Shim
+PRODUCT_PACKAGES += \
+    libshim_showlogo
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_props.mk
