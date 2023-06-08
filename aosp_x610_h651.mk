c1e8b5e63ca03c5583ae68f128044d8e6b2b98ec
AndroidHQ254 <d.murimi14@gmail.com>
2023-06-08 14:44:15 +0300

    PixelExprience-ify

--- aosp_x610_h651.mk
+++ aosp_x610_h651.mk
@@ -12,11 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from Infinix Note 6 device
 $(call inherit-product, device/infinix/x610_h651/device.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_x610_h651
+PRODUCT_NAME := aosp_x610_h651
 PRODUCT_DEVICE := x610_h651
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Note 6
@@ -34,6 +36,10 @@ BUILD_FINGERPRINT := Infinix/H651B/Infinix-X610:9/PPR1.180610.011/AB-200805V370:
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_x610_h651-user 9 PPR1.180610.011 31055 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="Infinix-X610" \
+    PRODUCT_NAME="Infinix-X610"
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
