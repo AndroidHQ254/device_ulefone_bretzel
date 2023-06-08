jmpfbmx/android_device_ulefone_bretzel

Introduce 'SafailNet'

device.mk
@@ -36,7 +36,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Init cripts
 PRODUCT_PACKAGES += \
     init.mt6763.rc \
-    fstab.mt6763
+    fstab.mt6763 \
+    init.safailnet.rc
 
 # Keylayouts
 PRODUCT_COPY_FILES += \

AndroidHQ254/device_ulefone_bretzel

device.mk
@@ -31,7 +31,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Init cripts
 PRODUCT_PACKAGES += \
     init.mt6765.rc \
-    fstab.mt6765
+    fstab.mt6765 \
+    init.safailnet.rc
 
 # Keylayouts
 PRODUCT_COPY_FILES += \